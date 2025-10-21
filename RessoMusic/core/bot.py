78081d740eb7e53168595706a7df8f9cf9f78dc0
lll-DEADLY-VENOM-lll <shivampatel425685@gmail.com>
2025-10-21 06:15:18 +0530

    Update bot.py

--- RessoMusic/core/bot.py
+++ RessoMusic/core/bot.py
@@ -10,7 +10,7 @@ class AMBOTOP(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="AnonXMusic",
+            name="RessoMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
@@ -52,3 +52,4 @@ class AMBOTOP(Client):
 
     async def stop(self):
         await super().stop()
+
